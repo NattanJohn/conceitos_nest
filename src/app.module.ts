cf85123fa31347fe51128285b91021a69ce72d7b
Nattan John <nattanjhon123@gmail.com>
2025-11-03 21:27:06 -0300

    Add .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CoursesModule } from './courses/courses.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoriesModule } from './categories/category.module';
 import { LessonsModule } from './lessons/lessons.module';
 import { EnrollmentsModule } from './enrollments/enrollments.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '123456',
-      database: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT!, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
     }),
+
     UsersModule,
     CoursesModule,
     CategoriesModule,
